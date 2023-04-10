3d438da9e6df121143d119c2cb4734f81e408dbb
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-04-10 19:30:17 -0300

    Se agregó tsconfig.json con sus configuraciones iniciales

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 var cadena = 'Hola Mundo';
-console.log(cadena);
+// console.log(cadena)
+var hero = {
+    name: 'Ironman',
+    age: 45
+};
+hero.age = 50;
+console.log(hero);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 
 const cadena: string = 'Hola Mundo';
-console.log(cadena)
+// console.log(cadena)
+
+const hero = {
+  name: 'Ironman',
+  age: 45
+}
+
+hero.age = 50
+
+console.log(hero)
