18910eb52a4054658b55375ce9654376749efd71
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-05-10 18:01:05 -0300

    Se realizan pruebas con el tipo de dato any y casting de datos

---
+++ fe-m01-bases/src/app_06_any.ts
@@ -0,0 +1,24 @@
+(()=>{
+  // Es recomendable usar lo menos posible el any
+  // con any tampoco tiene el ayuda "." y ver sus propiedades, podemos usarlas si las conocemos, pero ts no nos ayuda
+
+  let avenger: any = 123;
+  let hero: any = 'Superman'
+  let exist;
+  let power;
+
+  avenger = 'Dr Strange';
+  console.log(avenger.charAt(0)); // D
+
+  avenger = 150.23256415;
+  console.log(avenger.toFixed(2)); // 150.23
+
+
+  // casting, tratamos el valor como un string y nos muestra sus métodos y propiedades
+  console.log( (hero as string).charAt(0)); // S
+
+  // otro tipo de casteo
+  console.log((<string>hero).charAt(2)); // p
+
+
+})()
