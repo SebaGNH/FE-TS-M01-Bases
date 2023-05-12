bdb9ad1aaef5ecf86e4d82b9578afe7add73c9dd
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-05-12 20:29:13 -0300

    se agregan tipos de datos en arrays

---
+++ fe-m01-bases/src/app_07_arrays.ts
@@ -0,0 +1,11 @@
+(()=>{
+  // Si no lo definimos no aceptaría booleanos
+  const numeros: (string | number )[] = [1,2,3,4,5,"6",7,8,9];
+
+  console.log(numeros); // Array(9) [ 1, 2, 3, 4, 5, "6", 7, 8, 9 ]
+
+  const villanos = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
+
+  villanos.forEach( v => console.log(v.toLocaleUpperCase()));
+
+})()
