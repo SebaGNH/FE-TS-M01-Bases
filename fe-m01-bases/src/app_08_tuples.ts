b4eef23cc2b881af4c1d213c597d219963d8a600
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-05-15 21:07:47 -0300

    se agregan tuplas

---
+++ fe-m01-bases/src/app_08_tuples.ts
@@ -0,0 +1,12 @@
+(()=>{// El primer tipo de dato sea un "string" y el segundo "number"
+
+  const hero: [string, number, boolean] = ['Dr Strange', 100, true];
+
+  //hero[0] = 50; //Lo marca como error ya que aquí espera un string
+
+  hero[1] = 50;
+  hero[2] = false;
+
+  console.log(hero); // Array(3) [ "Dr Strange", 50, false ]
+
+})()
